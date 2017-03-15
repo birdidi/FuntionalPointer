aec5540d35f2ba9228986d2fb1247b7db88769bf
chenxy <374158795@qq.com>
2017-03-15 21:53:46 +0800

    commit service

---
+++ app/src/main/java/com/cxy/android/functionalpointer/service/FunctionalService.java
@@ -0,0 +1,17 @@
+package com.cxy.android.functionalpointer.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Author: xyc000
+ * Description: </br>
+ * Date: 2017/3/15 0015
+ */
+public class FunctionalService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
